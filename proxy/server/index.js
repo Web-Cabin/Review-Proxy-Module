67407472f69632b4f43f7cfbd798c6d61bc523fc
Daniel Kang <danieldonghoonkang@gmail.com>
2019-09-03 21:54:59 -0700

    changed PORT number

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = 3010;
 const app = express();
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, '../public')));
