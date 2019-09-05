83e306fc0d72e2f33298e80cff27d3b8c98e1cd8
Daniel Kang <danieldonghoonkang@gmail.com>
2019-09-04 20:22:35 -0700

    setting up proxy

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const PORT = 3010;
+const PORT = 3005;
 const app = express();
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-// app.get('/api/proxy', (req, res) => {
-//   res.send(console.log('hello, the get request went through'));
-// });
+app.get('/bundle.js', (req, res) => {
+  app.sendFile(path.resolve(__dirname, 'client/dist/bundle.js'), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('was able to complete .get from proxy server');
+    }
+  })
+});
 
 
 app.listen(PORT, () => {
